d07811273f53030bde9e0483410d6c7fb45f8b0f
Abreham Atlaw <abrishatlaw@gmail.com>
2022-02-22 04:18:44 +0300

    Removed neccassity of PSQL connection

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,9 @@
 import psycopg2 as pg
 
 from .Config import DEFAULT_PG_CONFIG
+from lib.utils.logger import Logger
 
-
-pg_connection = pg.connect(**DEFAULT_PG_CONFIG)
+try:
+	pg_connection = pg.connect(**DEFAULT_PG_CONFIG)
+except:
+	Logger.error("Couldn't Connect to Postgres")
