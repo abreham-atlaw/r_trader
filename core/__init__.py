628100959a7b81f94af9eab4b48f15dce83d33b4
Abreham Atlaw <abrishatlaw@gmail.com>
2022-02-22 04:25:16 +0300

    Fix bugs

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,8 @@
 import psycopg2 as pg
 
 from .Config import DEFAULT_PG_CONFIG
-from lib.utils.logger import Logger
 
 try:
 	pg_connection = pg.connect(**DEFAULT_PG_CONFIG)
 except:
-	Logger.error("Couldn't Connect to Postgres")
+	print("Couldn't Connect to Postgres")
