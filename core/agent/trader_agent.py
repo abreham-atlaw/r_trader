380d0de0d22bb5129a6e03d72ecab0b6f8e19f4f
Abreham Atlaw <abrishatlaw@gmail.com>
2022-05-27 05:50:06 +0300

    Temp Multiplier for delta model

--- core/agent/trader_agent.py
+++ core/agent/trader_agent.py
@@ -70,7 +70,7 @@ class TraderDNNTransitionAgent(DNNTransitionAgent, ABC):
 					sequence,
 					direction
 				).reshape((1, -1))
-			).flatten()[0]
+			).flatten()[0] * 1.8
 
 		else:
 			if isinstance(self.__state_change_delta, float):
