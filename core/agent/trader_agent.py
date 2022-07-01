9fdcf014c2ba84825693b6d3bf96761ab82c09c3
Abreham Atlaw <abrishatlaw@gmail.com>
2022-07-01 10:11:57 +0300

    Removed Ouput Multiplier

--- core/agent/trader_agent.py
+++ core/agent/trader_agent.py
@@ -70,7 +70,7 @@ class TraderDNNTransitionAgent(DNNTransitionAgent, ABC):
 					sequence,
 					direction
 				).reshape((1, -1))
-			).flatten()[0] * 1.8
+			).flatten()[0]
 
 		else:
 			if isinstance(self.__state_change_delta, float):
