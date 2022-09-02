dedc2b4495a6d767c1627e3cedeaf44eafbe4bbd
Abreham Atlaw <abrishatlaw@gmail.com>
2022-09-02 10:25:31 +0300

    Added Moving Average

--- lib/utils/math.py
+++ lib/utils/math.py
@@ -3,3 +3,11 @@ import numpy as np
 
 def sigmoid(x):
 	return 1/(1+np.e**(-x))
+
+
+def moving_average(x: np.ndarray, window_size: int):
+	data_len = len(x) - window_size + 1
+	x_final = np.zeros(data_len)
+	for i in range(data_len):
+		x_final[i] = np.mean(x[i: i + window_size])
+	return x_final
