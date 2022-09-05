035c387018390b4f943f4d34e39a7d7c64b22178
Abreham Atlaw <abrishatlaw@gmail.com>
2022-09-05 15:12:17 +0300

    Implemented FileIO, PickleFileIO and SerializerFileIO

---
+++ lib/utils/fileio.py
@@ -0,0 +1,46 @@
+from typing import *
+from abc import ABC, abstractmethod
+
+import pickle
+
+from lib.network.rest_interface.serializers import Serializer
+
+
+class FileIO(ABC):
+
+	@abstractmethod
+	def dumps(self, obj: Any , path: str):
+		pass
+
+	@abstractmethod
+	def loads(self, path: str) -> Any:
+		pass
+
+
+class PickleFileIO(FileIO):
+
+	def dumps(self, obj: Any, path: str):
+		with open(path, "wb") as file:
+			pickle.dump(obj, file)
+
+	def loads(self, path: str) -> Any:
+		file = open(path, "rb")
+		obj = pickle.load(file)
+		file.close()
+		return obj
+
+
+class SerializerFileIO(FileIO):
+
+	def __init__(self, serializer: Serializer):
+		self.__serializer = serializer
+
+	def dumps(self, obj: Any, path: str):
+		with open(path, "w") as file:
+			print(self.__serializer.serialize_json(obj), file=file)
+
+	def loads(self, path: str) -> Any:
+		file = open(path)
+		content = file.read()
+		file.close()
+		return self.__serializer.deserialize_json(content)
