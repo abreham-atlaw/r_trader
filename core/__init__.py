c0522498e555df223b8acffa398aa395ecc772e9
Abreham Atlaw <abrishatlaw@gmail.com>
2022-09-09 12:35:01 +0300

    Removed Logger Import from core __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,1 @@
-import psycopg2 as pg
 
-from .Config import DEFAULT_PG_CONFIG
-from lib.utils.logger import Logger
-
-try:
-	pg_connection = pg.connect(**DEFAULT_PG_CONFIG)
-except Exception as ex:
-	pg_connection = None
-	Logger.warning("Couldn't Connect to Postgres", ex)
