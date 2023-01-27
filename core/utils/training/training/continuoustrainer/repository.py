16d3e06b3012a2d438577e980da5861e14a668a7
Abreham Atlaw <abrishatlaw@gmail.com>
2023-01-27 12:22:57 +0300

    Migrated from epoch based to state based checkpoint

--- core/utils/training/training/continuoustrainer/repository.py
+++ core/utils/training/training/continuoustrainer/repository.py
@@ -2,8 +2,9 @@ import typing
 from abc import ABC, abstractmethod
 
 import os
+import json
 
-from lib.utils.file_storage import FileStorage, PCloudClient
+from lib.utils.file_storage import FileStorage, PCloudClient, LocalStorage
 from core.utils.training.training import Trainer
 from core import Config
 
@@ -11,17 +12,17 @@ from core import Config
 class TrainerRepository(ABC):
 
 	@abstractmethod
-	def update_checkpoint(self, id: str, paths: typing.Tuple[str, str], epoch: int):  # TODO: epoch: int  => state: Trainer.State
+	def update_checkpoint(self, id: str, paths: typing.Tuple[str, str], state: Trainer.State):  # TODO: epoch: int  => state: Trainer.State
 		pass
 
 	@abstractmethod
-	def get_checkpoint(self, id: str) -> typing.Optional[typing.Tuple[typing.Tuple[str, str], int]]:
+	def get_checkpoint(self, id: str) -> typing.Optional[typing.Tuple[typing.Tuple[str, str], Trainer.State]]:
 		pass
 
 
 class FileStorageTrainerRepository(TrainerRepository, ABC):
 
-	__DELIMITER = "\n"
+	PATH_KEY = "path-{}"
 
 	def __init__(self, base_path: str, *args, **kwargs):
 		super().__init__(*args, **kwargs)
@@ -30,26 +31,50 @@ class FileStorageTrainerRepository(TrainerRepository, ABC):
 	@abstractmethod
 	def _create_file_storage(self, base_path: str) -> FileStorage:
 		pass
+	@staticmethod
+	def __serialize_state(state: Trainer.State) -> typing.Dict:
+		return state.__dict__.copy()
 
-	def update_checkpoint(self, id: str, paths: typing.Tuple[str, str], epoch: int):
+	@staticmethod
+	def __deserialize_state(json_: typing.Dict) -> Trainer.State:
+		state = Trainer.State(None, None, None, None)
+		state.__dict__ = json_.copy()
+		return state
+
+	def update_checkpoint(self, id: str, paths: typing.Tuple[str, str], state: Trainer.State):
 		file = open(id, "w")
-		print(f"{paths[0]}{self.__DELIMITER}{paths[1]}{self.__DELIMITER}{epoch}", file=file, end="")
+
+		json_ = self.__serialize_state(state)
+		json_.update({
+			FileStorageTrainerRepository.PATH_KEY.format(i): path
+			for i, path in enumerate(paths)
+		})
+
+		json.dump(json_, file)
 		file.close()
 		self.__file_storage.upload_file(id)
 
-	def get_checkpoint(self, id: str) -> typing.Optional[typing.Tuple[typing.Tuple[str, str], int]]:
+	def get_checkpoint(self, id: str) -> typing.Optional[typing.Tuple[typing.Tuple[str, str], Trainer.State]]:
 		try:
 			checkpoint_url = self.__file_storage.get_url(id)
 		except Exception:
 			return None
 
 		os.system(f"wget {checkpoint_url} -O {id}")
-		with open(id) as file:
-			core_url, delta_url, epoch = file.read().split(self.__DELIMITER)
-		return (core_url, delta_url), int(epoch)
+		with open(id, "r") as file:
+			json_: typing.Dict = json.load(file)
+		paths: typing.Tuple[str, str] = tuple([json_.pop(FileStorageTrainerRepository.PATH_KEY.format(i)) for i in range(2)])
+		state = self.__deserialize_state(json_)
+		return paths, state
 
 
 class PCloudTrainerRepository(FileStorageTrainerRepository):
 
 	def _create_file_storage(self, base_path: str) -> FileStorage:
 		return PCloudClient(Config.PCLOUD_API_TOKEN, folder=base_path)
+
+
+class LocalFileStorageRepository(FileStorageTrainerRepository):
+
+	def _create_file_storage(self, base_path: str) -> FileStorage:
+		return LocalStorage(base_path, port=8000)
