ef1fd88e65f37d920d666865971c80b696d953b0
Abreham Atlaw <abrishatlaw@gmail.com>
2023-02-06 09:10:22 +0300

    Added get_all

--- core/utils/training/training/repository.py
+++ core/utils/training/training/repository.py
@@ -6,6 +6,14 @@ from pymongo import MongoClient
 
 class MetricRepository(ABC):
 
+	@staticmethod
+	def __construct_container(metrics: typing.List['Trainer.Metric']) -> 'Trainer.MetricsContainer':
+		from .trainer import Trainer
+		container = Trainer.MetricsContainer()
+		for m in metrics:
+			container.add_metric(m)
+		return container
+
 	@abstractmethod
 	def write_metric(self, metric: 'Trainer.Metric'):
 		pass
@@ -13,6 +21,13 @@ class MetricRepository(ABC):
 	@abstractmethod
 	def exists(self, metric: 'Trainer.Metric') -> bool:
 		pass
+
+	@abstractmethod
+	def _get_all(self) -> typing.List['Trainer.Metric']:
+		pass
+
+	def get_all(self) -> 'Trainer.MetricContainer':
+		return self.__construct_container(self._get_all())
 
 
 class MongoDBMetricRepository(MetricRepository):
@@ -28,4 +43,15 @@ class MongoDBMetricRepository(MetricRepository):
 	def exists(self, metric: 'Trainer.Metric') -> bool:
 		response = self._collection.find_one(metric.__dict__)
 		return response is not None
+
+	@staticmethod
+	def __construct_metric(json_):
+		from .trainer import Trainer
+		metric = Trainer.Metric(None, None, None, None, None)
+		for k in metric.__dict__.keys():
+			metric.__dict__[k] = json_[k]
+		return metric
+
+	def _get_all(self) -> typing.List['Trainer.Metric']:
+		return [self.__construct_metric(j) for j in list(self._collection.find())]
 
