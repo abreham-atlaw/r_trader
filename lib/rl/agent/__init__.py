a07c74c7a1f56daf15514792514b3412d28c9755
Abreham Atlaw <abrishatlaw@gmail.com>
2023-02-27 11:04:32 +0300

    Import

--- lib/rl/agent/__init__.py
+++ lib/rl/agent/__init__.py
@@ -1,5 +1,6 @@
-from .agent import Agent
+from .action_choice_agent import ActionChoiceAgent
 from .dta import DNNTransitionAgent
 from .mba import ModelBasedAgent
 from .markov_agent import MarkovAgent
 from .mca import MonteCarloAgent
+from .agent import Agent
