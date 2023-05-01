f73baaa50ddc40dc0af7e6ecd875a67abddf5bfd
Abreham Atlaw <abrishatlaw@gmail.com>
2023-05-01 19:38:20 +0300

    Added LocalStorage

--- lib/utils/file_storage/__init__.py
+++ lib/utils/file_storage/__init__.py
@@ -1,2 +1,2 @@
-from .file_storages import FileStorage, PCloudClient, DropboxClient
+from .file_storages import FileStorage, PCloudClient, DropboxClient, LocalStorage
 from .combined_file_storages import CombinedFileStorage, PCloudCombinedFileStorage
