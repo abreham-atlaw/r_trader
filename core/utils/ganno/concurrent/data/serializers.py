74c910f518da449cc2260effa4d96ebdd75f20e2
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-06-03 11:49:55 +0300

    Updated Serializers:

--- core/utils/ganno/concurrent/data/serializers.py
+++ core/utils/ganno/concurrent/data/serializers.py
@@ -3,7 +3,7 @@ from typing import *
 from tensorflow.keras import losses, optimizers, activations
 
 from lib.network.rest_interface import Serializer
-from core.utils.ganno.nnconfig import NNConfig, ModelConfig, ConvPoolLayer
+from core.utils.ganno.nnconfig import NNConfig, ModelConfig, ConvPoolLayer, KalmanFiltersConfig, TransformerConfig
 
 
 class ConvPoolLayerSerializer(Serializer):
@@ -20,11 +20,44 @@ class ConvPoolLayerSerializer(Serializer):
 		return obj
 
 
+class KalmanFilterConfigSerializer(Serializer):
+
+	def __init__(self):
+		super().__init__(KalmanFiltersConfig)
+
+	def serialize(self, data: object) -> Dict:
+		return data.__dict__.copy()
+
+	def deserialize(self, json_: Dict) -> object:
+		obj = KalmanFiltersConfig(None, None)
+		obj.__dict__ = json_.copy()
+		return obj
+
+
+class TransformerConfigSerializer(Serializer):
+
+	def __init__(self):
+		super().__init__(TransformerConfig)
+
+	def serialize(self, data: TransformerConfig) -> Dict:
+		json_ = data.__dict__.copy()
+		json_["dense_activation"] = activations.serialize(data.dense_activation)
+		return json_
+
+	def deserialize(self, json_: Dict) -> TransformerConfig:
+		obj = TransformerConfig(None, None, None, None, None)
+		obj.__dict__ = json_.copy()
+		obj.dense_activation = activations.deserialize(obj.dense_activation)
+		return obj
+
+
 class ModelConfigSerializer(Serializer):
 
 	def __init__(self):
 		super().__init__(ModelConfig)
 		self.__conv_serializer = ConvPoolLayerSerializer()
+		self.__kalmanconfigserializer = KalmanFilterConfigSerializer()
+		self.__transformer_serializer = TransformerConfigSerializer()
 
 	def serialize(self, data: ModelConfig) -> Dict:
 		json = data.__dict__.copy()
@@ -32,6 +65,8 @@ class ModelConfigSerializer(Serializer):
 			self.__conv_serializer.serialize(layer)
 			for layer in data.ff_conv_pool_layers
 		]
+		json["kalman_filters"] = self.__kalmanconfigserializer.serialize(data.kalman_filters)
+		json["transformer_config"] = self.__transformer_serializer.serialize(data.transformer_config)
 		json["loss"] = losses.serialize(data.loss)
 		json["optimizer"] = optimizers.serialize(data.optimizer)
 		json["dense_activation"] = activations.serialize(data.dense_activation)
@@ -46,6 +81,8 @@ class ModelConfigSerializer(Serializer):
 			self.__conv_serializer.deserialize(layer)
 			for layer in config.ff_conv_pool_layers
 		]
+		config.kalman_filters = self.__kalmanconfigserializer.deserialize(config.kalman_filters)
+		config.transformer_config = self.__transformer_serializer.deserialize(config.transformer_config)
 		config.loss = losses.deserialize(config.loss)
 		config.optimizer = optimizers.deserialize(config.optimizer)
 		config.dense_activation = activations.deserialize(config.dense_activation)
