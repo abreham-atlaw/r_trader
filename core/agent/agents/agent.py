5da78d5a352a978d8561d785c119d11a8ee139b8
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-07-04 12:32:24 +0300

    Made ActionChoiceTrader default

--- core/agent/agents/agent.py
+++ core/agent/agents/agent.py
@@ -1,9 +1,10 @@
 
 
 from .action_choice_trader import ActionChoiceBalancerTrader
+from .action_choice_trader.action_choice_trader import ActionChoiceTrader
 from .dnn_transition_agent import TraderDNNTransitionAgent
 from .montecarlo_agent import TraderMonteCarloAgent
 
 
-class TraderAgent(ActionChoiceBalancerTrader, TraderDNNTransitionAgent, TraderMonteCarloAgent):
+class TraderAgent(TraderDNNTransitionAgent, TraderMonteCarloAgent, ActionChoiceTrader):
 	pass
