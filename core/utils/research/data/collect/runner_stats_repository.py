11369ad28e1132ea436b4dc9db7526598b9f6610
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-11-22 10:06:33 +0300

    Implemented RunnerStatsRepository

---
+++ core/utils/research/data/collect/runner_stats_repository.py
@@ -0,0 +1,44 @@
+import typing
+from dataclasses import dataclass
+
+from pymongo import MongoClient
+
+
+@dataclass
+class RunnerStats:
+	id: str
+	model_url: str
+	value: float = 0.0
+	duration: float = 0.0
+	accuracy: float = 0.0
+
+
+class RunnerStatsRepository:
+
+	def __init__(
+			self,
+			client: MongoClient,
+			db_name: str = "runner_stats",
+			collection_name: str = "runner_stats"
+	):
+		db = client[db_name]
+		self._collection = db[collection_name]
+
+	def store(self, stats: RunnerStats):
+		old_stats = self.retrieve(stats.id)
+		if old_stats is None:
+			self._collection.insert_one(
+				stats.__dict__
+			)
+			return
+		old_stats += stats.duration
+		old_stats.value = stats.value
+		self._collection.update_one(
+			{"id": old_stats.id},
+			{"$set": old_stats.__dict__},
+			upsert=True
+		)
+
+	def retrieve(self, id: str) -> typing.Optional[RunnerStats]:
+		doc = self._collection.find_one({"id": id})
+		return RunnerStats(**doc) if doc else None
