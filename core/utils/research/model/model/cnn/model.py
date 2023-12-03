Ganno Initial population change. softmax addition on wrapped, minor bug fixes

core/utils/research/model/model/cnn/model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class CNN(nn.Module):
 
-	def __init__(self, num_classes, conv_channels, kernel_sizes, pool_sizes, dropout_rate):
+	def __init__(self, num_classes, conv_channels, kernel_sizes, pool_sizes, dropout_rate, kaiming_normal=False):
 		super(CNN, self).__init__()
 		self.layers = nn.ModuleList()
 		for i in range(len(conv_channels) - 1):
@@ -12,6 +12,8 @@ class CNN(nn.Module):
 				nn.Conv1d(in_channels=conv_channels[i], out_channels=conv_channels[i+1], kernel_size=kernel_sizes[i], stride=1, padding=1),
 				nn.ReLU()
 			]
+			if kaiming_normal:
+				nn.init.kaiming_normal_(layers[0].weight, mode='fan_out', nonlinearity='relu')
 			if pool_sizes[i] > 0:
 				layers.append(nn.MaxPool1d(kernel_size=pool_sizes[i], stride=2))
 			if dropout_rate > 0:
@@ -19,6 +21,8 @@ class CNN(nn.Module):
 			self.layers.append(nn.Sequential(*layers))
 		self.avg_pool = nn.AdaptiveAvgPool1d((1,))
 		self.fc = nn.Linear(conv_channels[-1], num_classes)
+		if kaiming_normal:
+			nn.init.kaiming_normal_(self.fc.weight, mode='fan_out', nonlinearity='relu')
 		if dropout_rate > 0:
 			self.dropout = nn.Dropout(dropout_rate)
 		else:

core/utils/research/model/model/wrapped.py
@@ -10,7 +10,7 @@ class WrappedModel(nn.Module):
 		super().__init__(*args, **kwargs)
 		self.model = model
 		self.ma = MovingAverage(window_size)
-		self.softmax = nn.Softmax()
+		self.softmax = nn.Softmax(dim=-1)
 
 	def forward(self, inputs: torch.Tensor):
 		inputs = self.ma(inputs)

test/core/utils/ganno/torch/optimizer_test.py
@@ -3,10 +3,11 @@ import unittest
 from torch.utils.data import DataLoader
 
 from core import Config
+from core.utils.ganno.torch.concurrent.data.serializers import CNNConfigSerializer
 from core.utils.ganno.torch.optimizer import Optimizer, CNNOptimizer, TransformerOptimizer
 from core.utils.research.data.load.dataset import BaseDataset
-from core.utils.research.training.callbacks.checkpoint_callback import StoreCheckpointCallback
-from lib.utils.file_storage import PCloudClient
+from lib.ga.callbacks import CheckpointCallback, StoreCheckpointCallback
+from lib.utils.file_storage import PCloudClient, DropboxClient
 
 
 class OptimizerTest(unittest.TestCase):
@@ -24,9 +25,17 @@ class OptimizerTest(unittest.TestCase):
 		)
 
 		trainer_callbacks = [
+			# StoreCheckpointCallback(
+			# 	path="/home/abreham/Projects/PersonalProjects/RTrader/r_trader/temp/models/",
+			# 	fs=PCloudClient(Config.PCLOUD_API_TOKEN, "/Apps/RTrader/Models/Collected/")
+			# )
+		]
+
+		callbacks = [
 			StoreCheckpointCallback(
-				path="/home/abreham/Projects/PersonalProjects/RTrader/r_trader/temp/models/",
-				fs=PCloudClient(Config.PCLOUD_API_TOKEN, "/Apps/RTrader/Models/Collected/")
+				fs=PCloudClient(Config.PCLOUD_API_TOKEN, "/Apps/RTrader/GA/"),
+				species_serializer=CNNConfigSerializer(),
+				save_path="/home/abreham/Projects/PersonalProjects/RTrader/r_trader/temp/configs/population.ga"
 			)
 		]
 
@@ -34,12 +43,12 @@ class OptimizerTest(unittest.TestCase):
 			vocab_size=449,
 			dataset=dataset,
 			test_dataset=test_dataset,
-			epochs=2,
+			epochs=1,
 			population_size=5,
-			trainer_callbacks=trainer_callbacks
+			trainer_callbacks=trainer_callbacks,
 		)
 
-		optimizer.start(epochs=10)
+		optimizer.start(epochs=3, callbacks=callbacks)
 
 	def test_transformer_optimizer(self):
 		dataset = BaseDataset(