f3a5df841ad786767af97c936ed2de6a4d0df33e
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-12-13 05:07:08 +0300

    Used Leaky Relu

--- core/utils/research/model/model/linear/model.py
+++ core/utils/research/model/model/linear/model.py
@@ -5,12 +5,17 @@ import torch.nn as nn
 
 
 class LinearModel(nn.Module):
-	def __init__(self, block_size: int, vocab_size: int, layer_sizes: typing.List[int], dropout_rate: float):
+	def __init__(self, block_size: int, vocab_size: int, layer_sizes: typing.List[int], dropout_rate: float, leaky_relu=True):
 		super(LinearModel, self).__init__()
 		self.layers = nn.ModuleList()
 		layer_sizes = [block_size] + layer_sizes + [vocab_size]
 		for i in range(len(layer_sizes) - 1):
 			self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+			if not leaky_relu:
+				nn.init.kaiming_uniform_(self.layers[-1].weight, nonlinearity='relu')
+		self.activation = nn.ReLU()
+		if leaky_relu:
+			self.activation = nn.LeakyReLU()
 		if dropout_rate > 0:
 			self.dropout = nn.Dropout(dropout_rate)
 		else:
@@ -19,6 +24,6 @@ class LinearModel(nn.Module):
 	def forward(self, x):
 		for layer in self.layers:
 			x = layer(x)
-			x = torch.relu(x)
+			x = self.activation(x)
 			x = self.dropout(x)
 		return x
