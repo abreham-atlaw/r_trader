b1aa1e34edb10fc176655ca2a0a3aaefc5de6478
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-12-14 09:17:08 +0300

    Added tanh and xavier support

--- core/utils/research/model/model/linear/model.py
+++ core/utils/research/model/model/linear/model.py
@@ -1,21 +1,29 @@
 import typing
 
-import torch
 import torch.nn as nn
 
 
 class LinearModel(nn.Module):
-	def __init__(self, block_size: int, vocab_size: int, layer_sizes: typing.List[int], dropout_rate: float, leaky_relu=True):
+	def __init__(self, block_size: int, vocab_size: int, layer_sizes: typing.List[int], dropout_rate: float, activation='relu', init='kaiming'):
 		super(LinearModel, self).__init__()
 		self.layers = nn.ModuleList()
 		layer_sizes = [block_size] + layer_sizes + [vocab_size]
 		for i in range(len(layer_sizes) - 1):
 			self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
-			if not leaky_relu:
-				nn.init.kaiming_uniform_(self.layers[-1].weight, nonlinearity='relu')
-		self.activation = nn.ReLU()
-		if leaky_relu:
+			if init == 'kaiming':
+				nn.init.kaiming_uniform_(self.layers[-1].weight, nonlinearity=activation)
+			elif init == 'xavier':
+				nn.init.xavier_uniform_(self.layers[-1].weight)
+			else:
+				raise ValueError("Invalid initialization function. Valid initializations are 'kaiming' and 'xavier'.")
+		if activation == 'relu':
+			self.activation = nn.ReLU()
+		elif activation == 'leaky_relu':
 			self.activation = nn.LeakyReLU()
+		elif activation == 'tanh':
+			self.activation = nn.Tanh()
+		else:
+			raise ValueError("Invalid activation function. Valid activations are 'relu', 'leaky_relu' and 'tanh'.")
 		if dropout_rate > 0:
 			self.dropout = nn.Dropout(dropout_rate)
 		else:
