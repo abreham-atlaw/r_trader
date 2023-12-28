2f57fe49b29316d71bd7ad40a87c95a0dc1263e8
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-12-28 08:55:13 +0300

    Added Norm

--- core/utils/research/model/model/linear/model.py
+++ core/utils/research/model/model/linear/model.py
@@ -11,12 +11,21 @@ class LinearModel(nn.Module):
 			layer_sizes: typing.List[int],
 			dropout_rate: float = 0,
 			activation: typing.Optional[nn.Module] = None,
-			init_fn: typing.Optional[typing.Callable] = None
+			init_fn: typing.Optional[typing.Callable] = None,
+			norm: typing.Union[bool, typing.List[bool]] = False
 	):
 		super(LinearModel, self).__init__()
-		self.layers = nn.ModuleList()
 		layer_sizes = [block_size] + layer_sizes + [vocab_size]
+
+		if isinstance(norm, bool):
+			norm = [norm for _ in range(len(layer_sizes))]
+		if len(norm) != len(layer_sizes)-1:
+			raise ValueError("Norm size doesn't match layers size")
+		self.layers = nn.ModuleList()
+
 		for i in range(len(layer_sizes) - 1):
+			if norm[i]:
+				self.layers.append(nn.LayerNorm(layer_sizes[i]))
 			self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
 			if init_fn is not None:
 				init_fn(self.layers[-1].weight)
