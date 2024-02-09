11007dcd6493ce4d92ad7725fcff2a3d3d9637d6
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-02-09 10:24:42 +0300

    Progress

--- lib/rl/agent/drmca/drmca.py
+++ lib/rl/agent/drmca/drmca.py
@@ -6,13 +6,15 @@ from lib.rl.agent.dra.dra import DeepReinforcementAgent
 
 class DeepReinforcementMonteCarloAgent(MonteCarloAgent, DeepReinforcementAgent, ABC):
 
+	def __init__(self, *args, wp: float = 50, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.__wp = wp
+
 	def _get_action_node_value(self, node: 'MonteCarloAgent.Node'):
 		calculated_value = super()._get_action_node_value(node)
 		predicted_value = super(DeepReinforcementAgent, self)._get_state_action_value(
 			self._state_repository.retrieve(node.parent.id),
 			node.action
 		)
-		self._getroo
-		return (node.visits*)
-
-
+		value = ((self.__wp - node.visits)*predicted_value + node.visits*calculated_value)/self.__wp
+		return value
