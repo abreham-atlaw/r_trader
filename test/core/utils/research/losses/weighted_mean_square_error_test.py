3f0a9cd4309a00b63a3629cc7fe7efb35842183a
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-02-26 09:11:42 +0300

    Adopted Change

--- test/core/utils/research/losses/weighted_mean_square_error_test.py
+++ test/core/utils/research/losses/weighted_mean_square_error_test.py
@@ -9,9 +9,9 @@ class WeightedMSELossTest(unittest.TestCase):
 
 
 	def test_functionality(self):
-		y = torch.asarray([[0, 1, 0, 0]])
-		y_hat = torch.asarray([[0.1, 0.1, 0.7, 0.1]])
+		y = torch.asarray([[0, 1, 0, 0], [0, 1, 0, 0]])
+		y_hat = torch.asarray([[0, 0.9, 0.1, 0], [0, 1, 0, 0]])
 
-		loss = WeightedMSELoss(4)
-		e = loss(y, y_hat)
+		loss = WeightedMSELoss(4, a=0.001)
+		e = loss(y_hat, y)
 		self.assertIsNotNone(e)
