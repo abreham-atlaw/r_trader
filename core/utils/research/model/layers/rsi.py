9443b1b587496d2d4980aa55ada8cf2c4099e9fe
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-05-16 10:55:42 +0300

    Adapted to New OverlayIndicator

--- core/utils/research/model/layers/rsi.py
+++ core/utils/research/model/layers/rsi.py
@@ -6,7 +6,6 @@ from .overlay_indicator import OverlayIndicator
 from .delta import Delta
 from .sign import SignFilter
 
-
 class RelativeStrengthIndex(OverlayIndicator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,13 +14,13 @@ class RelativeStrengthIndex(OverlayIndicator):
         self.__loss_filter = SignFilter(-1)
 
     def _on_time_point(self, inputs: torch.Tensor) -> torch.Tensor:
-        percentage = self.__delta(inputs) / inputs[:, :-1]
+        percentage = self.__delta(inputs) / inputs[:, :, :-1]
         average_gain = torch.mean(
             self.__gain_filter(percentage),
-            dim=1
+            dim=2
         )
         average_loss = torch.mean(
             -1 * self.__loss_filter(percentage),
-            dim=1
+            dim=2
         )
         return 1 - (1 / (1 + average_gain / average_loss))
