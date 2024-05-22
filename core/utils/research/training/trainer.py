823ed6e19d247e6ce0ed1375b5c5b9bc442073c9
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-05-22 11:50:42 +0300

    Restored initialization" ;

--- core/utils/research/training/trainer.py
+++ core/utils/research/training/trainer.py
@@ -31,7 +31,7 @@ class Trainer:
         if callbacks is None:
             callbacks = []
 
-        # self.model = self.__initialize_model(model)
+        self.model = self.__initialize_model(model)
         self.cls_loss_function = cls_loss_function
         self.reg_loss_function = reg_loss_function
         self.optimizer = optimizer
