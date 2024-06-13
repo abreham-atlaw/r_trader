cb3169125e27467fb2033caf0107928831e18d7d
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-06-13 09:40:37 +0300

    Implemented Flatten Collapse

--- core/utils/research/model/model/cnn/model.py
+++ core/utils/research/model/model/cnn/model.py
@@ -25,6 +25,7 @@ class CNN(SavableModule):
 			padding: int = 1,
 			avg_pool=True,
 			linear_collapse=False,
+			flatten_collapse=False,
 			input_size: int = 1028,
 			norm: typing.Union[bool, typing.List[bool]] = False,
 	):
@@ -44,7 +45,8 @@ class CNN(SavableModule):
 			'linear_collapse': linear_collapse,
 			'input_size': input_size,
 			'norm': norm,
-			'indicators': indicators
+			'indicators': indicators,
+			'flatten_collapse': flatten_collapse
 		}
 		self.extra_len = extra_len
 		self.layers = nn.ModuleList()
@@ -106,8 +108,11 @@ class CNN(SavableModule):
 			self.dropout = nn.Dropout(dropout_rate)
 		else:
 			self.dropout = nn.Identity()
-
+		self.ff_linear = ff_linear
+		self.fc_layer = None
+		self.num_classes = num_classes
 		self.collapse_layer = None if linear_collapse else nn.AdaptiveAvgPool1d((1,))
+		self.flatten_collapse = flatten_collapse
 		self.__init()
 
 	def __init(self):
@@ -115,9 +120,23 @@ class CNN(SavableModule):
 		self(init_data)
 
 	def collapse(self, out: torch.Tensor) -> torch.Tensor:
+		if self.flatten_collapse:
+			return torch.flatten(out, 1, 2)
 		if self.collapse_layer is None:
 			self.collapse_layer = nn.Linear(out.shape[-1], 1)
 		return self.collapse_layer(out)
+
+	def fc(self, out: torch.Tensor) -> torch.Tensor:
+		if self.fc_layer is None:
+			if self.ff_linear is None:
+				self.fc_layer = nn.Linear(out.shape[-1], self.num_classes)
+			else:
+				self.fc_layer = nn.Sequential(
+					nn.Linear(out.shape[-1], self.ff_linear.input_size),
+					self.ff_linear,
+					nn.Linear(self.ff_linear.output_size, self.num_classes)
+				)
+		return self.fc_layer(out)
 
 	def forward(self, x):
 		seq = x[:, :-self.extra_len]
