c4b5fcf255e7db3d81f4508415bdf3307c15c681
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-08-06 17:06:02 +0300

    Unified OUTPATH

--- core/Config.py
+++ core/Config.py
@@ -227,8 +227,7 @@ DUMP_CANDLESTICKS_PATH = None
 UPDATE_TRAIN = False
 AGENT_CRA_SIZE = 0
 
-MODEL_IN_PATH = "/Apps/RTrader/maploss/models/cnn"
-MODEL_OUT_PATH = "/Apps/RTrader/maploss/models(evaluated)/cnn"
+MODEL_OUT_PATH = "/Apps/RTrader/maploss/models/cnn"
 MODEL_TMP_PATH = os.path.abspath("./out/")
 
 TIMEOUT = 3*60*60
