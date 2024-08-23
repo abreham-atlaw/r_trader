0bc021cfafb2b2d73d15bbb25809defd880ae46d
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-08-23 20:21:21 +0300

    Device Bug

--- core/utils/research/losses/rwl.py
+++ core/utils/research/losses/rwl.py
@@ -29,12 +29,13 @@ class ReverseMAWeightLoss(ReverseWeightLoss):
 	def __init__(self, window_size: int, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.window = window_size
+		self.weights = torch.ones(1, 1, self.window).to(self.device)
 
 	def generate_weights(self, s: torch.Tensor) -> torch.Tensor:
 		s = F.pad(s, (self.window - 1, self.window - 1))
 
 		for _ in range(2):
-			s = F.conv1d(s.unsqueeze(1), torch.ones(1, 1, self.window) / self.window).squeeze(dim=1)
+			s = F.conv1d(s.unsqueeze(1),  self.weights/ self.window).squeeze(dim=1)
 
 		s = s / torch.max(s, dim=1).values
 
