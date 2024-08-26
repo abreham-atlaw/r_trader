0b3992fcf00d54284aa321bb4d23bf97a47e9f7a
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-08-26 13:16:40 +0300

    Added Sorting for consistency

--- core/utils/research/data/prepare/duplicate_data_cleaner.py
+++ core/utils/research/data/prepare/duplicate_data_cleaner.py
@@ -1,3 +1,4 @@
+import os.path
 import typing
 
 import numpy as np
@@ -17,7 +18,9 @@ class DuplicateDataCleaner:
 		for arr, path in zip((X, y), (X_file, y_file)):
 			np.save(path, arr)
 
-	def start(self, X_files: typing.List[str], y_files: typing.List[str] = None):
+	def start(self, X_files: typing.List[str], y_files: typing.List[str] = None, sort: bool = True):
+		if sort:
+			X_files, y_files = [sorted(files, key=lambda file: os.path.basename(file)) for files in (X_files, y_files)]
 		for i, (X_file, y_file) in enumerate(zip(X_files, y_files)):
 			self.__clean_file(X_file, y_file)
 			print(f"Completed: {(i+1)*100/len(X_files) :.2f}%", end="\r")
