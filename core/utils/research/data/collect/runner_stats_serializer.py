2d889b0c0ee8c060dff6ca4d4b12e8c07afa98af
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-09-26 07:44:54 +0300

    Serialization Fix

--- core/utils/research/data/collect/runner_stats_serializer.py
+++ core/utils/research/data/collect/runner_stats_serializer.py
@@ -10,7 +10,10 @@ class RunnerStatsSerializer(Serializer):
 		super().__init__(RunnerStats)
 
 	def serialize(self, data: RunnerStats) -> Dict:
-		return data.__dict__.copy()
+		json_ = data.__dict__.copy()
+		if "profit" in json_.keys():
+			json_.pop("profit")
+		return json_
 
 	def deserialize(self, json_: Dict) -> RunnerStats:
 		if json_.get("_id"):
@@ -18,6 +21,9 @@ class RunnerStatsSerializer(Serializer):
 
 		if "profits" not in json_.keys():
 			json_["profits"] = [json_.pop("profit")] + [0 for _ in range(len(json_["session_timestamps"]) - 1)]
+
+		if "profit" in json_.keys():
+			json_.pop("profit")
 
 		json_["model_losses"] = tuple(json_["model_losses"])
 		return RunnerStats(**json_)
