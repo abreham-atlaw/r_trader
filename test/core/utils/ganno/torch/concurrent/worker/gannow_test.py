dd07a1ca9a70d9f6f502648fcf467d38a78fb464
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-09-28 08:48:30 +0300

    Test Modification

--- test/core/utils/ganno/torch/concurrent/worker/gannow_test.py
+++ test/core/utils/ganno/torch/concurrent/worker/gannow_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+from core import Config
 from core.utils.ganno.torch.concurrent.queen.gannoq import CNNOptimizerQueen
 from core.utils.ganno.torch.concurrent.worker.gannow import CNNOptimizerWorker
 from core.utils.research.data.load.dataset import BaseDataset
@@ -7,27 +8,26 @@ from core.utils.research.data.load.dataset import BaseDataset
 
 class GannoWorkerTest(unittest.TestCase):
 
-	URL = "https://ga-server.vercel.app/"
-
 	def test_functionality(self):
 		dataset = BaseDataset(
 			[
-				"/home/abreham/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared_actual/train"
+				"/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/train"
 			],
 		)
 		test_dataset = BaseDataset(
 			[
-				"/home/abreham/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared_actual/test"
+				"/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/train"
 			],
 		)
 
 		worker = CNNOptimizerWorker(
-			self.URL,
+			Config.GANNO_SERVER_URL,
 			vocab_size=449,
 			dataset=dataset,
 			test_dataset=test_dataset,
 			epochs=2,
-			population_size=5
+			population_size=5,
+			train_timeout=10
 		)
 
 		worker.start(
