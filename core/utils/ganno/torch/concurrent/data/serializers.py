d8131b1c1b0d746856bd20b94a5bbdb619f23c0c
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-09-28 13:20:04 +0300

    Serializer Fix

--- core/utils/ganno/torch/concurrent/data/serializers.py
+++ core/utils/ganno/torch/concurrent/data/serializers.py
@@ -2,8 +2,26 @@ import json
 
 import typing
 
-from core.utils.ganno.torch.nnconfig import ConvLayer, CNNConfig
+from core.utils.ganno.torch.nnconfig import ConvLayer, CNNConfig, LinearConfig
 from lib.network.rest_interface import Serializer
+
+
+class LinearConfigSerializer(Serializer):
+
+	def __init__(self):
+		super().__init__(LinearConfig)
+
+	def serialize(self, data: LinearConfig) -> typing.Dict:
+		return data.__dict__.copy()
+
+	def serialize_json(self, data: LinearConfig):
+		return json.dumps(self.serialize(data))
+
+	def deserialize(self, json_: typing.Dict) -> LinearConfig:
+		return LinearConfig(**json_)
+
+	def deserialize_json(self, json_: str) -> LinearConfig:
+		return self.deserialize(json.loads(json_))
 
 
 class ConvLayerSerializer(Serializer):
@@ -29,10 +47,12 @@ class CNNConfigSerializer(Serializer):
 	def __init__(self):
 		super().__init__(CNNConfig)
 		self.__conv_layer_serializer = ConvLayerSerializer()
+		self.__linear_block_serializer = LinearConfigSerializer()
 
 	def serialize(self, data: CNNConfig) -> typing.Dict:
-		data_dict = data.__dict__.copy()  # create a copy of data's dict
+		data_dict = data.__dict__.copy()
 		data_dict['layers'] = [self.__conv_layer_serializer.serialize(layer) for layer in data.layers]
+		data_dict["ff_block"] = self.__linear_block_serializer.serialize(data.ff_block)
 		return data_dict
 
 	def serialize_json(self, data: CNNConfig):
@@ -40,6 +60,7 @@ class CNNConfigSerializer(Serializer):
 
 	def deserialize(self, json_: typing.Dict) -> CNNConfig:
 		json_['layers'] = [self.__conv_layer_serializer.deserialize(layer) for layer in json_['layers']]
+		json_["ff_block"] = self.__linear_block_serializer.deserialize(json_["ff_block"])
 		return CNNConfig(**json_)
 
 	def deserialize_json(self, json_: str) -> CNNConfig:
