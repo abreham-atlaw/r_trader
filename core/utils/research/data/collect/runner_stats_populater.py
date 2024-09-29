366ed575ae4d8e793b420f79a619c731cdb01b66
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-09-29 16:32:15 +0300

    Added Losses

--- core/utils/research/data/collect/runner_stats_populater.py
+++ core/utils/research/data/collect/runner_stats_populater.py
@@ -58,15 +58,15 @@ class RunnerStatsPopulater:
 			)
 			for loss in [
 				nn.CrossEntropyLoss(),
-				# WeightedMSELoss(len(Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND) + 1),
-				# MeanSquaredClassError(
-				# 	Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND,
-				# 	Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND_EPSILON
-				# ),
-				# ReverseMAWeightLoss(window_size=5, softmax=True),
-				# ReverseMAWeightLoss(window_size=10, softmax=True),
-				# ReverseMAWeightLoss(window_size=20, softmax=True),
-				# ReverseMAWeightLoss(window_size=40, softmax=True),
+				WeightedMSELoss(len(Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND) + 1),
+				MeanSquaredClassError(
+					Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND,
+					Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND_EPSILON
+				),
+				ReverseMAWeightLoss(window_size=5, softmax=True),
+				ReverseMAWeightLoss(window_size=10, softmax=True),
+				ReverseMAWeightLoss(window_size=20, softmax=True),
+				ReverseMAWeightLoss(window_size=40, softmax=True),
 			]
 		])
 
