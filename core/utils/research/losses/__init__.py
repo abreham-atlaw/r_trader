909184d6314e78fb38705746f419b463c438cdbc
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-10-17 15:25:02 +0300

    Implemented and Tested PredictionConfidenceScoreLoss

--- core/utils/research/losses/__init__.py
+++ core/utils/research/losses/__init__.py
@@ -10,3 +10,4 @@ from .log_loss import LogLoss
 from .pml import ProximalMaskedLoss
 from .obvl import OutputBatchVarianceLoss
 from .obcv import OutputBatchClassVariance
+from .pcsl import PredictionConfidenceScoreLoss
