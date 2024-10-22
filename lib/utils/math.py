5481117f4b2f1c0e6f0b8a847e51ec28c7657f25
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-10-22 13:05:23 +0300

    Implemented counter_moving_average

--- lib/utils/math.py
+++ lib/utils/math.py
@@ -11,3 +11,8 @@ def moving_average(x: np.ndarray, window_size: int):
 	for i in range(data_len):
 		x_final[i] = np.mean(x[i: i + window_size])
 	return x_final
+
+
+def counter_moving_average(sequence: np.ndarray, value: float, window_size: int):
+	return (value * window_size) - np.sum(sequence[:, -window_size + 1:])
+
