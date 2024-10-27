dd56c8673e45076f8b6d8c5cb142432c5efa3961
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-10-27 13:06:01 +0300

    Added STM Branch

--- core/Config.py
+++ core/Config.py
@@ -230,12 +230,14 @@ class RunnerStatsBranches:
 	ma_ews = "ma_ews"
 	ma_ews_trim_scaling = "ma_ews_trim_scaling"
 	ma_ews_dynamic_k = "ma_ews_dynamic_k"
+	ma_ews_dynamic_k_stm = "ma_ews_dynamic_k_stm"
 
 	all = [
 		main,
 		ma_ews,
 		ma_ews_trim_scaling,
-		ma_ews_dynamic_k
+		ma_ews_dynamic_k,
+		ma_ews_dynamic_k_stm
 	]
 
 	default = main
