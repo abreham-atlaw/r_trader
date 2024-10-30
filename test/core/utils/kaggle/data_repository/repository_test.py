36abcf3fca22c3fa320bb7d8921c6e7d381675d8
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-10-30 12:18:04 +0300

    Implemented and Tested Dataset Download

--- test/core/utils/kaggle/data_repository/repository_test.py
+++ test/core/utils/kaggle/data_repository/repository_test.py
@@ -10,6 +10,8 @@ class KaggleDataRepositoryTest(unittest.TestCase):
 	def setUp(self):
 		self.output_path = "/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/notebook_exports/test"
 		self.slug = "abrehamatlaw0/mock-notebook-0"
+		self.dataset_slug = "arslaan5/global-data-gdp-life-expectancy-and-more"
+
 		self.repository = KaggleDataRepository(
 			output_path=self.output_path
 		)
@@ -17,6 +19,12 @@ class KaggleDataRepositoryTest(unittest.TestCase):
 	def test_download(self):
 		self.repository.download()
 		os.system(f"tree \"{self.output_path}\"")
+
+	def test_download_dataset(self):
+		self.repository.download(
+			slug=self.dataset_slug,
+			kernel=False
+		)
 
 	def test_checksum(self):
 		download_path = os.path.join(self.output_path, self.slug.replace("/", "-"))
