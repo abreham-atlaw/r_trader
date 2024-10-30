f2cea5b492867ee110109fce2767abf5774d6554
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-10-30 11:50:51 +0300

    Checksum Tests

--- test/core/utils/kaggle/data_repository/repository_test.py
+++ test/core/utils/kaggle/data_repository/repository_test.py
@@ -9,11 +9,19 @@ class KaggleDataRepositoryTest(unittest.TestCase):
 
 	def setUp(self):
 		self.output_path = "/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/notebook_exports/test"
+		self.slug = "abrehamatlaw0/mock-notebook-0"
 		self.repository = KaggleDataRepository(
 			output_path=self.output_path
 		)
 
 	def test_download(self):
-		self.repository.download("abrehamatlaw0/mock-notebook-0")
+		self.repository.download()
 		os.system(f"tree \"{self.output_path}\"")
+
+	def test_checksum(self):
+		download_path = os.path.join(self.output_path, self.slug.replace("/", "-"))
+		self.repository.download("abrehamatlaw0/mock-notebook-0")
+		checksum = self.repository.generate_checksum(download_path)
+		print(f"Checksum {checksum}")
+		self.repository.download(self.slug, checksum=checksum)
 
