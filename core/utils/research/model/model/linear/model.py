a58d8bd2ed1cc03bbaa5175752de8aab90f8e68a
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-12-08 02:01:56 +0300

    Fixed Scalar Norm bug

--- core/utils/research/model/model/linear/model.py
+++ core/utils/research/model/model/linear/model.py
@@ -38,7 +38,7 @@ class LinearModel(SpinozaModule):
 
 		self.norms_mask = norm
 		if isinstance(norm, bool):
-			self.norms_mask = [norm for _ in range(len(self.layer_sizes) - 1)]
+			self.norms_mask = [norm for _ in range(len(self.layers_sizes) - 1)]
 		if len(self.norms_mask) != (len(self.layers_sizes) - 1):
 			raise ValueError("Norm size doesn't match layers size")
 		self.norm_learnable = norm_learnable
