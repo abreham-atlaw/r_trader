55d672f570bc2bd5da428e9bd42098d24789da17
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-12-28 12:14:18 +0300

    Implemented and Tested RESNet using CNN Architecture

--- core/utils/research/model/model/cnn/residual_block.py
+++ core/utils/research/model/model/cnn/residual_block.py
@@ -1,21 +1,17 @@
-
+import torch
 import torch.nn as nn
 
 
 class ResidualBlock(nn.Module):
-	def __init__(self, in_channels, out_channels, kernel_size, padding, hidden_activation, init_fn=None, norm=True):
+	def __init__(self, in_channels, out_channels, kernel_size, padding, init_fn=None, norm=False, hidden_activation=None):
 		super(ResidualBlock, self).__init__()
 		self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
 		self.norm1 = nn.BatchNorm1d(out_channels) if norm else nn.Identity()
-		self.activation = hidden_activation
 		self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
 		self.norm2 = nn.BatchNorm1d(out_channels) if norm else nn.Identity()
+		self.activation = hidden_activation if hidden_activation is not None else nn.Identity()
 		self.init_fn = init_fn
-
-		if in_channels != out_channels:
-			self.shortcut = nn.Conv1d(in_channels, out_channels, kernel_size=1)
-		else:
-			self.shortcut = nn.Identity()
+		self.shortcut = nn.Conv1d(in_channels, out_channels, kernel_size=1) if in_channels != out_channels else nn.Identity()
 
 		if init_fn is not None:
 			init_fn(self.conv1.weight)
@@ -39,6 +35,5 @@ class ResidualBlock(nn.Module):
 		out = self.norm2(out)
 
 		out = self.residual(identity, out)
-		out = self.activation(out)
 
 		return out
