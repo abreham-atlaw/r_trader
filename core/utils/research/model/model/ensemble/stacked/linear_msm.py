3cf9f3a491508b95845f0487a1d91747b716a99d
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-01-27 13:10:55 +0300

    Added Bias

--- core/utils/research/model/model/ensemble/stacked/linear_msm.py
+++ core/utils/research/model/model/ensemble/stacked/linear_msm.py
@@ -13,22 +13,25 @@ class LinearMSM(MaskedStackedModel):
 			self,
 			*args,
 			ff: typing.Optional[nn.Module] = None,
+			bias: bool = False,
 			**kwargs
 	):
 		super().__init__(*args, **kwargs)
 		self.args.update({
-			"ff": ff
+			"ff": ff,
+			"bias": bias
 		})
 		if ff is None:
 			ff = nn.Identity()
 
 		self.ff = ff
 		self.collapse_layer = None
+		self.bias = bias
 		self.init()
 
 	def collapse(self, x, y):
 		if self.collapse_layer is None:
-			self.collapse_layer = nn.Linear(x.shape[1], y.shape[1], bias=False)
+			self.collapse_layer = nn.Linear(x.shape[1], y.shape[1], bias=self.bias)
 		return self.collapse_layer(x)
 
 	def _get_mask(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
