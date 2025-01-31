45a06756d7845c132e70e29a7e2608bb828b76d0
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-01-31 17:41:22 +0300

    Enabled Trackers

--- core/di/legacy/research_provider.py
+++ core/di/legacy/research_provider.py
@@ -39,22 +39,22 @@ class ResearchProvider:
 
 	@staticmethod
 	def provide_default_trackers(model_name: str) -> typing.List['TorchTracker']:
-		return []
-		# from core.utils.research.training.trackers.stats_tracker import DynamicStatsTracker
-		# from core.utils.research.training.trackers.stats_tracker import Keys, WeightsStatsTracker, GradientsStatsTracker
-		#
-		# return [
-		# 	DynamicStatsTracker(
-		# 		model_name=model_name,
-		# 		label=key
-		# 	)
-		# 	for key in Keys.ALL
-		# ] + [
-		# 	WeightsStatsTracker(
-		# 		model_name=model_name
-		# 	),
-		# 	GradientsStatsTracker(
-		# 		model_name=model_name
-		# 	)
-		# ]
-		#
+		# return []
+		from core.utils.research.training.trackers.stats_tracker import DynamicStatsTracker
+		from core.utils.research.training.trackers.stats_tracker import Keys, WeightsStatsTracker, GradientsStatsTracker
+
+		return [
+			DynamicStatsTracker(
+				model_name=model_name,
+				label=key
+			)
+			for key in Keys.ALL
+		] + [
+			WeightsStatsTracker(
+				model_name=model_name
+			),
+			GradientsStatsTracker(
+				model_name=model_name
+			)
+		]
+
