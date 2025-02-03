d45feca26445fb5f912d0da1102547f46923fa6f
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-02-03 14:53:01 +0300

    Revert

--- core/utils/research/model/model/ensemble/stacked/encdec_fusion/encdec_fusion_model.py
+++ core/utils/research/model/model/ensemble/stacked/encdec_fusion/encdec_fusion_model.py
@@ -3,7 +3,6 @@ import typing
 import torch
 from torch import nn
 
-from core.utils.research.model.layers import DynamicLayerNorm
 from .edf_decoder import EDFDecoder
 
 from core.utils.research.model.model.ensemble.stacked.stacked_ensemble_model import StackedEnsembleModel
@@ -17,21 +16,18 @@ class EncDecFusionModel(StackedEnsembleModel):
 			models: typing.List[SpinozaModule],
 			encoder: nn.Module,
 			decoder: typing.Union[nn.Module, EDFDecoder],
-			ff: nn.Module,
-			preconcat_norm: bool = False,
+			ff: nn.Module
 	):
 		super().__init__(models)
 		self.args.update({
 			"encoder": encoder,
 			"decoder": decoder,
-			"ff": ff,
-			"preconcat_norm": preconcat_norm
+			"ff": ff
 		})
 		self.encoder = encoder
 		self.decoder = decoder
 		self.ff = ff
 		self.collapse_layer = None
-		self.encoder_norm, self.decoder_norm = [DynamicLayerNorm() if preconcat_norm else nn.Identity() for _ in range(2)]
 		self.init()
 
 	def collapse(self, y: torch.Tensor) -> torch.Tensor:
@@ -42,9 +38,6 @@ class EncDecFusionModel(StackedEnsembleModel):
 	def _call(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
 		encoded = self.encoder(x)
 		decoded = self.decoder(y)
-
-		encoded = self.encoder_norm(encoded)
-		decoded = self.decoder_norm(decoded)
 
 		concatenated = torch.cat([encoded, decoded], dim=1)
 
