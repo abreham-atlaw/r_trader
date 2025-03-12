2616eae075dd83eacb6917b85994562649b5681e
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-03-12 10:26:10 +0300

    Implemented and integrated market state anchor

--- core/environment/trade_state/market_state.py
+++ core/environment/trade_state/market_state.py
@@ -16,14 +16,16 @@ class MarketState:
 			state=None,
 			spread_state=None,
 			memory_len=None,
-			tradable_pairs=None
+			tradable_pairs=None,
+			anchor: 'MarketState' = None
 	):
+		self.__anchor = anchor
 
 		self.__currencies = currencies
 		if currencies is None:
 			self.__currencies = Config.CURRENCIES
 
-		if state is None and memory_len is None:
+		if state is None and anchor is None and memory_len is None:
 			raise Exception("Insufficient information given on State.")
 
 		if state is None:
@@ -44,10 +46,20 @@ class MarketState:
 		if spread_state is None:
 			self.__spread_state = np.zeros((len(currencies), len(currencies))).astype('float64')
 
+	@property
+	def is_anchored(self) -> bool:
+		return self.__anchor is not None
+
 	def __init_state(self, num_currencies, memory_len) -> np.ndarray:
-		state = np.zeros((num_currencies, num_currencies, memory_len)).astype('float64')
+
+		if self.is_anchored:
+			state = np.zeros((num_currencies, num_currencies, 0)).astype('float64')
+		else:
+			state = np.zeros((num_currencies, num_currencies, memory_len)).astype('float64')
+
 		for i in range(num_currencies):
 			state[i, i] = 1
+
 		return state
 
 	def __get_currencies_position(self, base_currency, quote_currency):
@@ -60,18 +72,34 @@ class MarketState:
 
 	def get_state_of(self, base_currency, quote_currency) -> np.ndarray:
 		bci, qci = self.__get_currencies_position(base_currency, quote_currency)
-		return self.__state[bci, qci]
+		state = self.__state[bci, qci]
+		if self.is_anchored:
+			state = np.concatenate((self.__anchor.get_state_of(base_currency, quote_currency),  state))[state.shape[0]:]
+		return state
 
 	def get_current_price(self, base_currency, quote_currency) -> np.float32:
+		"""
+		TODO: OPTIMIZE. If self.is_anchored and self.__state.shape[-1] > 0 return self.__state[bci, qci, -1]
+		TODO: (no need to acquire and concatenate anchor.get_state_of(...)
+		"""
 		return self.get_state_of(base_currency, quote_currency)[-1]
 
 	def get_spread_state_of(self, base_currency, quote_currency) -> float:
 		bci, qci = self.__get_currencies_position(base_currency, quote_currency)
 		return self.__spread_state[bci, qci]
 
+	def __add_empty_state_layer(self, size: int):
+		self.__state = np.concatenate((self.__state, np.zeros((self.__state.shape[0], self.__state.shape[1], size))), axis=2)
+
 	def update_state_of(self, base_currency, quote_currency, values: np.ndarray):
 		bci, qci = self.__get_currencies_position(base_currency, quote_currency)
-		self.__state[bci, qci] = np.concatenate((self.__state[bci, qci, len(values):], values))
+
+		if self.is_anchored:
+			self.__add_empty_state_layer(len(values))
+			self.__state[bci, qci, -len(values):] = values
+		else:
+			self.__state[bci, qci] = np.concatenate((self.__state[bci, qci, len(values):], values))
+
 		self.__state[qci, bci] = 1/self.__state[bci, qci]
 
 	def update_state_layer(self, state_layer: np.ndarray):
@@ -79,8 +107,11 @@ class MarketState:
 			for j in range(state_layer.shape[1]):
 				if not np.isclose(state_layer[i, j], 1/state_layer[j, i]):
 					Logger.warning(f"Inconsistent Layer given. {state_layer[i, j], state_layer[j, i]}")
+		if self.is_anchored:
+			self.__add_empty_state_layer(1)
+		else:
+			self.__state[:, :, :-1] = self.__state[:, :, 1:]
 
-		self.__state[:, :, :-1] = self.__state[:, :, 1:]
 		self.__state[:, :, -1] = state_layer
 
 		for i in range(len(self.__currencies)):
@@ -95,12 +126,14 @@ class MarketState:
 		self.__spread_state = spread_state
 
 	def get_price_matrix(self) -> np.ndarray:
-		return self.__state
+		return self.__state  # TODO: INCLUDE ANCHORED STATE AS WELL
 
 	def get_spread_matrix(self) -> np.ndarray:
 		return self.__spread_state
 
 	def get_memory_len(self) -> int:
+		if self.is_anchored:
+			return self.__anchor.get_price_matrix().shape[2]
 		return self.__state.shape[2]
 
 	def get_currencies(self) -> List[str]:
@@ -113,11 +146,16 @@ class MarketState:
 		return value * self.get_current_price(from_, to)
 
 	def __deepcopy__(self, memo=None):
+		if Config.MARKET_STATE_USE_ANCHOR:
+			state, anchor = None, self
+		else:
+			state, anchor = self.__state.copy(), None
 		return MarketState(
 			currencies=self.__currencies.copy(),
 			tradable_pairs=self.__tradable_pairs.copy(),
-			state=self.__state.copy(),
-			spread_state=self.__spread_state.copy()
+			state=state,
+			spread_state=self.__spread_state.copy(),
+			anchor=anchor
 		)
 
 	def __hash__(self):
