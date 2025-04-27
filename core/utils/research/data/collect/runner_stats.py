4a3e06ebb8016a2a0cd0e0ca2546346de140575f
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-04-27 08:53:23 +0300

    - Added Session Model Loss - Implemented and Tested Session Model Evaluation

--- core/utils/research/data/collect/runner_stats.py
+++ core/utils/research/data/collect/runner_stats.py
@@ -17,6 +17,7 @@ class RunnerStats:
 	duration: float = 0.0
 	model_losses_map: typing.Dict[str, typing.Tuple[float, ...]] = field(default_factory=lambda: {})
 	temperature: float = 1.0
+	session_model_losses: typing.List[float] = field(default_factory=lambda: [])
 
 	@property
 	def profit(self) -> float:
@@ -43,3 +44,9 @@ class RunnerStats:
 
 	def add_session_timestamp(self, timestamp: datetime):
 		self.session_timestamps.append(timestamp)
+
+	def add_session_model_loss(self, loss: float):
+		if len(self.session_model_losses) < (len(self.session_timestamps) - 1):
+			self.session_model_losses.extend([0.0] * (len(self.session_timestamps) - 1 - len(self.session_model_losses)))
+
+		self.session_model_losses.append(loss)
--- test/core/utils/research/data/collect/sim_setup/rs_setup_manager_test.py
+++ test/core/utils/research/data/collect/sim_setup/rs_setup_manager_test.py
@@ -3,24 +3,36 @@ import unittest
 
 from core import Config
 from core.di import ResearchProvider
+from core.utils.research.data.collect.sim_setup.rs_setup_manager import RSSetupManager
 
 
 class RSSetupManagerTest(unittest.TestCase):
 
 	def setUp(self):
 
-		Config.RunnerStatsBranches.default = Config.RunnerStatsBranches.ma_ews_dynamic_k_stm_it_27
-		Config.RunnerStatsLossesBranches.default = Config.RunnerStatsLossesBranches.it_27
+		Config.RunnerStatsBranches.default = Config.RunnerStatsBranches.it_23_0
+		Config.RunnerStatsLossesBranches.default = Config.RunnerStatsLossesBranches.it_23
 		Config.OANDA_SIM_MODEL_IN_PATH = "/Apps/RTrader/maploss/it-27/"
 
 		Config.OANDA_TRADING_ACCOUNT_ID = ""
 		Config.OANDA_TRADING_URL = "http://127.0.0.1:8888/api"
 
-		self.setup_manager = ResearchProvider.provide_rs_setup_manager()
+		self.setup_manager: RSSetupManager = ResearchProvider.provide_rs_setup_manager()
 
 	def test_setup(self):
 		stat = self.setup_manager.setup()
 
 		self.assertNotEqual(Config.OANDA_TRADING_ACCOUNT_ID, "")
 		self.assertEqual(stat.model_name, os.path.basename(Config.CORE_MODEL_CONFIG.path))
+		print(stat)
+
+	def test_setup_and_finish(self):
+		stat = self.setup_manager.setup()
+
+		PL = 1.2
+		self.setup_manager.finish(
+			stat=stat,
+			pl=PL,
+		)
+
 		print(stat)
