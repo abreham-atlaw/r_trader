7ac56692c4d24245eaaa374a42df99394573d3fd
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-05-01 11:18:45 +0300

    Adopted unbatched_model_evaluator.py to weight

--- test/core/utils/research/utils/unbatched_model_evaluator_test.py
+++ test/core/utils/research/utils/unbatched_model_evaluator_test.py
@@ -1,9 +1,11 @@
 import unittest
 
+import numpy as np
 import torch
 
 from core import Config
 from core.utils.research.eval.mlpl_evaluator.losses.unbatched_pml_loss import UnbatchedProximalMaskedLoss
+from core.utils.research.losses import ProximalMaskedLoss
 from core.utils.research.utils.unbatched_model_evaluator import UnbatchedModelEvaluator
 from lib.utils.torch_utils.model_handler import ModelHandler
 
@@ -12,9 +14,10 @@ class UnbatchedModelEvaluatorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.evaluator = UnbatchedModelEvaluator(
-			loss_fn=UnbatchedProximalMaskedLoss(
+			cls_loss_fn=ProximalMaskedLoss(
 				n=len(Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND) + 1,
-				p=1
+				p=1,
+				collapsed=False
 			),
 			data_path="/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/4/test"
 		)
@@ -25,5 +28,5 @@ class UnbatchedModelEvaluatorTest(unittest.TestCase):
 		losses = self.evaluator.evaluate(self.model)
 		print(losses)
 
-		self.assertIsInstance(losses, torch.Tensor)
+		self.assertIsInstance(losses, np.ndarray)
 		self.assertEqual(losses.shape[0], 128)
