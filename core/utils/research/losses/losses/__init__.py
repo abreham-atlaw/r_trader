ca8d324714cca2d7847df778ef080ec0ab4d20d4
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-05-01 07:39:57 +0300

    - Implemented cross_entropy.py

--- core/utils/research/losses/losses/__init__.py
+++ core/utils/research/losses/losses/__init__.py
@@ -2,3 +2,4 @@ from .msce import MeanSquaredClassError
 from .pml import ProximalMaskedLoss
 from .rwl import ReverseMAWeightLoss, ReverseWeightLoss
 from .mse import MeanSquaredErrorLoss
+from .cross_entropy import CrossEntropyLoss
