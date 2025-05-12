55e63cbdb89272aa2b25c30e4b67790c7e0707a7
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-05-12 20:45:59 +0300

    Added it-23-sw-8

--- core/Config.py
+++ core/Config.py
@@ -300,6 +300,7 @@ class RunnerStatsLossesBranches:
 	it_23_sw_5 = "it_23_sw_5"
 	it_23_sw_6 = "it_23_sw_6"
 	it_23_sw_7 = "it_23_sw_7"
+	it_23_sw_8 = "it_23_sw_8"
 	it_24 = "it_24"
 	it_27 = "it_27"
 	it_29 = "it_29"
@@ -318,6 +319,7 @@ class RunnerStatsLossesBranches:
 		it_23_sw_5,
 		it_23_sw_6,
 		it_23_sw_7,
+		it_23_sw_8,
 		it_24,
 		it_27,
 		it_29,
