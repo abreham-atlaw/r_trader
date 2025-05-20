9b3394c79bad546c32fcfe7844e3255cf116cb0c
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-05-20 07:13:02 +0300

    - Implemented and Tested basic_swmn.py

---
+++ core/utils/research/data/prepare/swg/manipulate/abstract_swmn.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+import os
+
+from lib.utils.logger import Logger
+
+
+class AbstractSampleWeightManipulator(ABC):
+
+	def __init__(self, weights_path: str, export_path: str):
+		self.__weights_path = weights_path
+		self.__export_path = export_path
+
+	def __setup(self):
+		os.makedirs(self.__export_path, exist_ok=True)
+
+	@abstractmethod
+	def _manipulate(self, w: np.ndarray) -> np.ndarray:
+		pass
+
+	def start(self):
+		self.__setup()
+
+		filenames = sorted(os.listdir(self.__weights_path))
+
+		for i, filename in enumerate(filenames):
+			weights = np.load(os.path.join(self.__weights_path, filename))
+			manipulated_weights = self._manipulate(weights)
+			np.save(os.path.join(self.__export_path, filename), manipulated_weights)
+			Logger.info(f"[+]Processed {(i + 1) * 100 / len(filenames):.2f}%", end="\r")
+
+		Logger.info(f"Done!")
---
+++ test/core/utils/research/data/prepare/swg/manipulate/basic_swmn_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from core.utils.research.data.prepare.swg.manipulate import BasicSampleWeightManipulator
+
+
+class BasicSampleWeightManipulatorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.manipulator = BasicSampleWeightManipulator(
+			weights_path="/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/4/test/w",
+			export_path="/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/4/test/w_manipulated"
+		)
+
+	def test_functionality(self):
+		self.manipulator.start()
