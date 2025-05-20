d0ba9ccf7325a2261aeda4d28f40cafc8af6ecc6
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-05-20 07:24:38 +0300

    - Further testing on basic_swmn_test.py

--- test/core/utils/research/data/prepare/swg/manipulate/basic_swmn_test.py
+++ test/core/utils/research/data/prepare/swg/manipulate/basic_swmn_test.py
@@ -8,7 +8,10 @@ class BasicSampleWeightManipulatorTest(unittest.TestCase):
 	def setUp(self):
 		self.manipulator = BasicSampleWeightManipulator(
 			weights_path="/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/4/test/w",
-			export_path="/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/4/test/w_manipulated"
+			export_path="/home/abrehamatlaw/Projects/PersonalProjects/RTrader/r_trader/temp/Data/prepared/4/test/w_manipulated",
+			shift=3,
+			scale=2,
+			stretch=10
 		)
 
 	def test_functionality(self):
