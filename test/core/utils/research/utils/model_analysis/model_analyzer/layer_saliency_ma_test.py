d81080ca163bca7ab6518cc949335b8f1184c48c
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-05-25 07:56:12 +0300

    - Implemented and Tested layer_saliency_ma.py

---
+++ test/core/utils/research/utils/model_analysis/model_analyzer/layer_saliency_ma_test.py
@@ -0,0 +1,21 @@
+from torch import nn as nn
+
+from core.utils.research.data.load import BaseDataset
+from core.utils.research.utils.model_analysis.model_analyzer import ModelAnalyzer
+from core.utils.research.utils.model_analysis.model_analyzer.layer_saliency_ma import LayerSaliencyModelAnalyzer
+from test.core.utils.research.utils.model_analysis.model_analyzer.abstract_model_analyzer_test import \
+	AbstractModelAnalyzerTest
+
+
+class LayerSaliencyModelAnalyzerTest(AbstractModelAnalyzerTest):
+
+	def _init_analyzer(self, model: nn.Module, dataset: BaseDataset) -> ModelAnalyzer:
+		return LayerSaliencyModelAnalyzer(
+			model=model,
+			dataset=dataset,
+			sample_size=10,
+			layers={
+				"Layer 0": model.layers[0],
+				"Layer 1": model.layers[1],
+			}
+		)
