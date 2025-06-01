0c3dccb6a8bec2a396672d9f3e37719a10fdf127
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-06-01 10:20:25 +0300

    - Implemented and Tested input_norm in collapse_block.py

--- core/utils/research/model/model/cnn/collapse_block.py
+++ core/utils/research/model/model/cnn/collapse_block.py
@@ -14,21 +14,26 @@ class CollapseBlock(SpinozaModule):
 		self,
 		ff_block: LinearModel = None,
 		dropout: float = 0,
-		extra_mode: bool = True
+		extra_mode: bool = True,
+		input_norm: typing.Optional[nn.Module] = None
 	):
 		super().__init__(auto_build=False)
 		self.args = {
 			'ff_block': ff_block,
 			'dropout': dropout,
-			"extra_mode": extra_mode
+			"extra_mode": extra_mode,
+			"input_norm": input_norm
 		}
 		self.ff_block = ff_block
 		self.dropout = nn.Dropout(dropout) if dropout > 0 else nn.Identity()
 		self.extra_mode = extra_mode
 		self.flatten = FlattenLayer(1, 2)
+		self.input_norm = input_norm if input_norm is not None else nn.Identity()
 
 	def call(self, x: torch.Tensor, extra: typing.Optional[torch.Tensor] = None) -> torch.Tensor:
-		flattened = self.flatten(x)
+		normed = self.input_norm(x)
+
+		flattened = self.flatten(normed)
 		flattened = flattened.reshape(flattened.size(0), -1)
 		flattened = self.dropout(flattened)
 
