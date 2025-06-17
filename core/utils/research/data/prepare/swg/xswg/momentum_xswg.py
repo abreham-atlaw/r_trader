815b47cce2969a650fc9dc5617844f54f8f0ba1f
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-06-17 17:13:04 +0300

    - Fixed all zero output.

--- core/utils/research/data/prepare/swg/xswg/momentum_xswg.py
+++ core/utils/research/data/prepare/swg/xswg/momentum_xswg.py
@@ -5,15 +5,24 @@ from core.utils.research.data.prepare.swg.xswg.basic_xswg import BasicXSampleWei
 
 class MomentumXSampleWeightGenerator(BasicXSampleWeightGenerator):
 
-	def __init__(self, *args, lookback: int = 5, **kwargs):
+	def __init__(
+			self,
+			*args,
+			lookback: int = 5,
+			X_extra_len: int = 124,
+			**kwargs
+	):
 		super().__init__(*args, **kwargs)
 		self.__lookback = lookback
+		self.__X_extra_len = X_extra_len
 
 	@staticmethod
 	def __delta(X: np.ndarray) -> np.ndarray:
 		return X[:, 1:] - X[:, :-1]
 
 	def _generate(self, X: np.ndarray) -> np.ndarray:
+		X = X[:, :-self.__X_extra_len]
+
 		return (
 			np.mean(np.abs(self.__delta(X[:, -self.__lookback:])), axis=1)
 			/
