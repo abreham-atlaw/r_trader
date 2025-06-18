cf4e7993d59a4af0014e6abe0c0f5f4be7eb6b70
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-06-18 06:19:06 +0300

    - Implemented performance_grid_msm.py

---
+++ core/utils/research/model/model/ensemble/stacked/msm/performance_grid_msm.py
@@ -0,0 +1,41 @@
+import typing
+
+import numpy as np
+import torch
+from torch import nn
+
+from core.utils.research.data.prepare.swg.xswg import BasicXSampleWeightGenerator
+from .masked_stacked_model import MaskedStackedModel
+
+
+class PerformanceGridMSM(MaskedStackedModel):
+
+	def __init__(
+			self,
+			generators: typing.List[BasicXSampleWeightGenerator],
+			performance_grid: typing.Union[np.ndarray, torch.Tensor],
+			*args,
+			**kwargs
+	):
+		self.args.update({
+			"generators": generators,
+			"performance_grid": performance_grid
+		})
+		super().__init__(*args, **kwargs)
+		self.generators = generators
+		self.performance_grid = torch.transpose(torch.from_numpy(performance_grid), 1, 0)
+		self.weights_softmax = nn.Softmax(dim=1)
+
+	def __get_weights(self, x: torch.Tensor) -> torch.Tensor:
+		weights = torch.stack([
+			torch.from_numpy(generator(x))
+			for generator in self.generators
+		], dim=1)
+		weights = self.weights_softmax(weights)
+		return weights
+
+	def _get_mask(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+		w = self.__get_weights(x)
+		mask = (w @ self.performance_grid)/w.shape[1]
+		mask = self.weights_softmax(mask)
+		return mask
