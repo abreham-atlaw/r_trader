7654d686b9c43c735980ccfcfea6c4262ddbb827
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-06-19 14:38:07 +0300

    - Implemented l1_norm.py

--- core/utils/research/model/layers/__init__.py
+++ core/utils/research/model/layers/__init__.py
@@ -18,3 +18,4 @@ from .flatten import FlattenLayer
 from .pass_through import PassThroughLayer
 from .min_max_norm import MinMaxNorm
 from .layer_stack import LayerStack
+from .l1_norm import L1Norm
