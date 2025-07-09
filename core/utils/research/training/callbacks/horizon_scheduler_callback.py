efc3bdaee977b3a3141030e334d3fd2ce6922591
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-07-09 17:22:17 +0300

    - Implemented and Tested epoch steps in horizon_scheduler_callback.py

--- core/utils/research/training/callbacks/horizon_scheduler_callback.py
+++ core/utils/research/training/callbacks/horizon_scheduler_callback.py
@@ -5,17 +5,20 @@ from ...model.model.utils import HorizonModel
 
 class HorizonSchedulerCallback(Callback):
 
-	def __init__(self, epochs: int, start: float = 0, end: float = 0.9):
+	def __init__(self, epochs: int, start: float = 0, end: float = 0.9, step: int = 1):
 		super().__init__()
 		self.__epochs = epochs
 		self.__start = start
 		self.__end = end
 		self.__step = (self.__end - self.__start) / self.__epochs
-		Logger.info(f"HorizonSchedulerCallback: Using step {self.__step}")
+		self.__epoch_step = step
+		Logger.info(f"HorizonSchedulerCallback: Using step {self.__step}@{self.__epoch_step} epochs")
 
 	def on_epoch_end(self, model: HorizonModel, epoch: int, losses, logs=None):
 		if not isinstance(model, HorizonModel):
 			Logger.warning(f"HorizonSchedulerCallback can only be used with HorizonModel. Got {type(model)}")
+			return
+		if epoch % self.__epoch_step != 0:
 			return
 		h = min(self.__end, self.__start + self.__step * epoch)
 		Logger.info(f"HorizonSchedulerCallback: Setting horizon to {h}")
