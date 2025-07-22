fa4f4afbf7f61fa941683d2314cb442986f5681a
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-07-22 04:10:57 +0300

    - Added it-41-h1

--- core/Config.py
+++ core/Config.py
@@ -360,6 +360,7 @@ class RunnerStatsLossesBranches:
 	it_40 = "it_40"
 	it_41 = "it_41"
 	it_41_h_0 = "it_41_h_0"
+	it_41_h_1 = "it_41_h_1"
 	it_42 = "it_42"
 
 	all = [
@@ -393,6 +394,7 @@ class RunnerStatsLossesBranches:
 		it_40,
 		it_41,
 		it_41_h_0,
+		it_41_h_1,
 		it_42,
 	]
 
