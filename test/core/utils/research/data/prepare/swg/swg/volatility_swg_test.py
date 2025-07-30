237d100741f0d9d64b25020e648baea6d6823aac
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-07-30 17:01:33 +0300

    - Implemented volatility_swg.py using delta mode

--- test/core/utils/research/data/prepare/swg/swg/volatility_swg_test.py
+++ test/core/utils/research/data/prepare/swg/swg/volatility_swg_test.py
@@ -3,6 +3,7 @@ import unittest
 from core import Config
 from core.utils.research.data.prepare.swg.abstract_swg import AbstractSampleWeightGenerator
 from core.utils.research.data.prepare.swg.swg import VolatilitySampleWeightGenerator
+from core.utils.research.data.prepare.utils.data_prep_utils import DataPrepUtils
 from test.core.utils.research.data.prepare.swg.swg.basic_swg_test import BasicSampleWeightGeneratorTest
 
 
@@ -10,7 +11,6 @@ class VolatilitySampleWeightGeneratorTest(BasicSampleWeightGeneratorTest ,unitte
 
 	def _init_generator(self) -> AbstractSampleWeightGenerator:
 		return VolatilitySampleWeightGenerator(
-			bounds=Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND,
-			p=48
+			bounds=DataPrepUtils.apply_bound_epsilon(Config.AGENT_STATE_CHANGE_DELTA_STATIC_BOUND),
 		)
 
