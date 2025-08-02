d5188649608fae2efc11db8eba9a6f968160d511
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-08-02 13:56:01 +0300

    - Defaulted 07.json

--- core/Config.py
+++ core/Config.py
@@ -190,7 +190,7 @@ AGENT_MODEL_USE_TRANSITION_ONLY = True
 AGENT_MODEL_EXTRA_LEN = 124
 AGENT_MODEL_TEMPERATURE = 1
 AGENT_STATE_CHANGE_DELTA_STATIC_BOUND_EPSILON = 1e-5
-with open(os.path.join(BASE_DIR, "res/bounds/06.json"), "r") as file:
+with open(os.path.join(BASE_DIR, "res/bounds/07.json"), "r") as file:
 	AGENT_STATE_CHANGE_DELTA_STATIC_BOUND = sorted(list(json.load(file)))
 with open(os.path.join(BASE_DIR, "res/weights/02.json"), "r") as file:
 	AGENT_STATE_CHANGE_DELTA_STATIC_BOUND_WEIGHTS = sorted(list(json.load(file)))
