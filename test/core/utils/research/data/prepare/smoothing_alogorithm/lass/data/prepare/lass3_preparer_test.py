930ff01bb09209ef5f1b635439b8305620a76211
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-08-06 18:33:08 +0300

    - Implemented and Tested lass3_preparer.py

---
+++ test/core/utils/research/data/prepare/smoothing_alogorithm/lass/data/prepare/lass3_preparer_test.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+import os
+import unittest
+
+from core import Config
+from core.utils.research.data.prepare.smoothing_algorithm import MovingAverage
+from core.utils.research.data.prepare.smoothing_algorithm.lass.data.prepare.lass3_preparer import Lass3Preparer
+from core.utils.research.data.prepare.splitting import SequentialSplitter
+
+
+class Lass3PreparerTest(unittest.TestCase):
+
+	def setUp(self):
+		self.preparer = Lass3Preparer(
+			sa=MovingAverage(window_size=32),
+			shift=16,
+			block_size=32,
+			granularity=30,
+			batch_size=64,
+			output_path=os.path.join(Config.BASE_DIR, "temp/Data/lass/2"),
+			order_gran=True,
+			df=pd.read_csv(os.path.join(Config.BASE_DIR, "temp/Data/AUD-USD-10k.csv")),
+			splitter=SequentialSplitter(test_size=0.2)
+		)
+
+	def test_functionality(self):
+		self.preparer.start()
