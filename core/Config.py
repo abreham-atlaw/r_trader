ea86752ff33d522f4cbdb2de5fe370a7b0fdb60f
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-08-16 18:47:08 +0300

    - Added branches it-47 and it-48

--- core/Config.py
+++ core/Config.py
@@ -300,6 +300,9 @@ class RunnerStatsBranches:
 	it_45_6 = "it_45_6"
 	it_46_6 = "it_46_6"
 
+	it_47_6 = "it_47_6"
+	it_48_6 = "it_48_6"
+
 	all = [
 		main,
 		ma_ews_dynamic_k_stm_it_23,
@@ -336,7 +339,9 @@ class RunnerStatsBranches:
 		it_43_2,
 		it_44_2,
 		it_45_6,
-		it_46_6
+		it_46_6,
+		it_47_6,
+		it_48_6
 	]
 
 	default = main
@@ -382,6 +387,7 @@ class RunnerStatsLossesBranches:
 	it_43_1 = "it_43_1"
 	it_44_1 = "it_44_1"
 	it_45_0 = "it_45_0"
+	it_47_0 = "it_47_0"
 
 	all = [
 		main,
@@ -420,7 +426,8 @@ class RunnerStatsLossesBranches:
 		it_42,
 		it_43_1,
 		it_44_1,
-		it_45_0
+		it_45_0,
+		it_47_0
 	]
 
 	default = main
