58c5fae817dcb8ee495d45f1b1b4ca59843d36a2
Abreham Atlaw <abreham.atlaw@yahoo.com>
2025-09-22 05:48:03 +0300

    - Added Identity as parent

--- core/utils/research/model/layers/identity.py
+++ core/utils/research/model/layers/identity.py
@@ -1,11 +1,12 @@
 import typing
 
 import torch
+import torch.nn as nn
 
 from core.utils.research.model.model.savable import SpinozaModule
 
 
-class Identity(SpinozaModule):
+class Identity(SpinozaModule, nn.Identity):
 
 	def call(self, x: torch.Tensor) -> torch.Tensor:
 		return x
